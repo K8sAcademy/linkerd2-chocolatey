59115feb0ba6ef6057af4733b21997778489b2ed
Guy Barrette <guy@guybarrette.com>
2022-09-23 12:39:13 -0400

    Update to 2.12.1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url      = 'https://github.com/linkerd/linkerd2/releases/download/stable-2.12.0/linkerd2-cli-stable-2.12.0-windows.exe'
-$checksum64    = 'a277313802045344f55b18ffbf240b4c2e9d293bfed0d7b691a0e6cb976000e6'
+$url      = 'https://github.com/linkerd/linkerd2/releases/download/stable-2.12.1/linkerd2-cli-stable-2.12.1-windows.exe'
+$checksum64    = '2cfc8a995f45b414da29c738639dc6d19a99777ce02d55189e220e766497f23b'
 $checksumType64= 'sha256'
 
 Get-ChocolateyWebFile -PackageName "Linkerd2" -FileFullPath "$toolsDir\linkerd.exe" -Url $url -checksum64 $checksum64 -checksumType64 $checksumType64
