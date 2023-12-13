3dce45ed904568949527462d751529b3018d39f7
Guy Barrette <guy@guybarrette.com>
2023-12-13 06:48:37 -0500

    Update to use -Url64bit

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -5,4 +5,4 @@ $url      = 'https://github.com/linkerd/linkerd2/releases/download/stable-2.14.6
 $checksum64    = 'c4eb43ecba1185d76fd2c255d302ccc568dcbf62770fe2db4b2581cf2fa69530'
 $checksumType64= 'sha256'
 
-Get-ChocolateyWebFile -PackageName "Linkerd2" -FileFullPath "$toolsDir\linkerd.exe" -Url $url -checksum64 $checksum64 -checksumType64 $checksumType64
+Get-ChocolateyWebFile -PackageName "Linkerd2" -FileFullPath "$toolsDir\linkerd.exe" -Url64bit $url -checksum64 $checksum64 -checksumType64 $checksumType64
